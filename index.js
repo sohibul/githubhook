d56b7da3805b4e109d85a332ff2f69cfdd5ef45f
Muhammad S. Maromi <msmaromi@gmail.com>
2016-06-30 16:30:11 +0700

    update

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 
 app.post('/', (req, res) => {
-  console.log("Post success");
+  console.log("POST success");
   res.send(true);
 });
+
+app.get('/get-gan', (req, res) => {
+  console.log("GET success");
+  res.send("GET GAN")
+})
 
 app.listen(3001, function() {
   console.log("Server running...");
