094d5e346959f88871cba18c3347b2455c0b081d
Muhammad S. Maromi <msmaromi@gmail.com>
2016-06-30 16:17:01 +0700

    Update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use((req, res) => {
-  console.log("request", req.url);
+app.post('/', (req, res) => {
+  console.log("Post success");
+  res.send(true);
 });
 
 app.listen(3001, function() {
-  console.log("Server running...";
+  console.log("Server running...");
 });
